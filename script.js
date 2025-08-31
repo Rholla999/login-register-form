6da4c0b4a8b43ce50244507517d6dfa38f733cee
MEMBA, Aku <104360361+Rholla999@users.noreply.github.com>
2025-08-31 21:31:50 +0100

    animation added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const passwordInput = document.getElementById('password');
+const verifyPasswordInput = document.getElementById('verifyPassword');
+const errorMessage = document.querySelector('.error');
+const signUpEl = document.querySelector('signupEl');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -8,4 +12,29 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+
+
+
+verifyPasswordInput.addEventListener('input', () => {
+    if (verifyPasswordInput.value === '') {
+        errorMessage.style.display = 'none';
+        return;
+    }
+   
+    if (passwordInput.value !== verifyPasswordInput.value) {
+        errorMessage.textContent = 'Passwords do not match';
+        errorMessage.style.display = 'block';
+    } else {
+        errorMessage.style.display = 'none';
+    }
+});
+
+
+const overlayRight = document.querySelector('.overlay-right')
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+  overlayRight.style.display = "none";   // hide overlay-right
 });
